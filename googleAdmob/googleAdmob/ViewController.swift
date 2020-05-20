db2004ab71d65c9f70bcb1d7ab28ee82d0c81b85
akrolayer <almagestf532@gmail.com>
2020-05-21 00:10:29 +0900

    Initialize commit

---
+++ googleAdmob/googleAdmob/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  googleAdmob
+//
+//  Created by akrolayer on 2020/05/21.
+//  Copyright © 2020 akrolayer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
